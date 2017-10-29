777f30af5287e1689e687c74bc69ed10fb5e5d8a
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-10-29 16:04:27 -0400

    update

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ $('#contact-form').on('submit', e => {
     var name = $('#inputName').val().trim();
     var email = $('#inputEmail').val().trim();
     var message = $('#inputMessage').val().trim();
-
-    if (name.val("") || email.val("") || message.val("")) {
-        alert('Please enter your name, email, and message.');
-    } else {
-        $.post({url: "https://formspree.io/jbedilio@yahoo.com", data: $(this).serialize(), datatype: 'json'});
+    $.post({url: "https://formspree.io/jbedilio@yahoo.com", data: $(this).serialize(), datatype: 'json'});
         $('#inputName').val("");
         $('#inputEmail').val("");
         $('#inputMessage').val("");
         alert('Message sent.');
-    }
 });
